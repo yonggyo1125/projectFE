35578387387bd6ef7ddaf566dd556a9274252efe
to7485 <54658614+to7485@users.noreply.github.com>
2023-11-13 10:48:11 +0900

    초기 설정, helmet, i18n 설정

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 import reportWebVitals from './reportWebVitals';
+
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
