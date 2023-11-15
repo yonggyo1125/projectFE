416dbf3a9161ab738136412d36e7ac65b1862f43
yonggyo1125 <yonggyo00@kakao.com>
2023-11-15 12:18:59 +0900

    .

--- src/components/member/LoginForm.js
+++ src/components/member/LoginForm.js
@@ -1,6 +1,9 @@
 import { InputText } from '../commons/InputStyle';
 import { BigButton } from '../commons/ButtonStyle';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { FiLock, FiKey, FiUserPlus } from 'react-icons/fi';
+
 import styled from 'styled-components';
 
 const LoginText = styled(InputText)`
@@ -12,6 +15,7 @@ const LoginText = styled(InputText)`
 
 const FormBox = styled.form`
   width: 300px;
+  padding-bottom: 80px;
 `;
 
 const LoginForm = () => {
@@ -21,9 +25,20 @@ const LoginForm = () => {
     <FormBox>
       <LoginText type="text" placeholder={t('아이디')} />
       <LoginText type="password" placeholder={t('비밀번호')} />
-      <BigButton type="submit" size="medium">
+      <BigButton type="submit" size="medium" className="mt5">
         {t('로그인')}
       </BigButton>
+      <div className="links">
+        <Link to="/find_id">
+          <FiLock /> {t('아이디 찾기')}
+        </Link>
+        <Link to="/find_pw">
+          <FiKey /> {t('비밀번호 찾기')}
+        </Link>
+        <Link to="/join">
+          <FiUserPlus /> {t('회원가입')}
+        </Link>
+      </div>
     </FormBox>
   );
 };
