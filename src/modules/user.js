3a19928a87d3a7e9f41f79167346245fae1cdcc4
yonggyo1125 <yonggyo00@kakao.com>
2023-11-16 11:19:20 +0900

    .

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,8 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState, useCallback } from 'react';
 
 const UserContext = createContext({
-  isLogin: false,
-  userInfo: {},
+  state: {
+    isLogin: false,
+    userInfo: {},
+  },
+  actions: {
+    setIsLogin: null,
+    setUserInfo: null,
+  },
 });
+
+const UserProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
+  const value = {
+    state: { isLogin, userInfo },
+    actions: { setIsLogin, setUserInfo },
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+const { Consumer: UserConsumer } = UserContext;
+
+export { UserProvider, UserConsumer };
 
 export default UserContext;
