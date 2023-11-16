acc6cbe38ce336cdfaa0376770a7bee6dec2620d
yonggyo1125 <yonggyo00@kakao.com>
2023-11-16 11:36:42 +0900

    .

--- src/containers/member/LoginContainer.js
+++ src/containers/member/LoginContainer.js
@@ -1,17 +1,17 @@
 import LoginForm from '../../components/member/LoginForm';
-import { UserConsumer } from '../../modules/user';
-const LoginContainer = () => {
+
+const LoginContainer = ({ setIsLogin, setUserInfo }) => {
+  const handleClick = () => {
+    setIsLogin(true);
+    setUserInfo({ userNm: '이이름' });
+  };
+
   return (
     <>
-      <UserConsumer>
-        {({ actions }) => {
-          return (
-            <>
-              <LoginForm />
-            </>
-          );
-        }}
-      </UserConsumer>
+      <LoginForm />
+      <button type="button" onClick={handleClick}>
+        로그인 상태
+      </button>
     </>
   );
 };
