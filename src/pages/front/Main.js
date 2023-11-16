dc13f164e2b0befe122e1e4446a236d67c189e5a
yonggyo1125 <yonggyo00@kakao.com>
2023-11-16 11:44:13 +0900

    .

--- src/pages/front/Main.js
+++ src/pages/front/Main.js
@@ -1,21 +1,17 @@
 import LoginContainer from '../../containers/member/LoginContainer';
-import { UserConsumer } from '../../modules/user';
+import UserContext from '../../modules/user';
+import { useContext } from 'react';
 
 const Main = () => {
-  console.log('Main');
-  return (
-    <UserConsumer>
-      {({ state, actions }) =>
-        state.isLogin ? (
-          <h1>{state.userInfo.userNm}</h1>
-        ) : (
-          <LoginContainer
-            setIsLogin={actions.setIsLogin}
-            setUserInfo={actions.setUserInfo}
-          />
-        )
-      }
-    </UserConsumer>
+  const {
+    state: { isLogin, userInfo },
+    actions: { setIsLogin, setUserInfo },
+  } = useContext(UserContext);
+
+  return isLogin ? (
+    <h1>메인페이지 : {userInfo.userNm}</h1>
+  ) : (
+    <LoginContainer setIsLogin={setIsLogin} setUserInfo={setUserInfo} />
   );
 };
 
