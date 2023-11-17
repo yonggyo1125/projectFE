78cabde40999817adb41c38b0e979fd66be44f43
yonggyo1125 <yonggyo00@kakao.com>
2023-11-17 12:49:18 +0900

    .

--- src/containers/member/JoinContainer.js
+++ src/containers/member/JoinContainer.js
@@ -24,17 +24,33 @@ const JoinContainer = () => {
         confirmPassword: t('NotBlank_confirmPassword'),
         name: t('NotBlank_name'),
       };
-      const _errors = {};
 
+      const _errors = {};
+      let hasError = false; // 검증 실패 여부
       for (const field in requiredFields) {
         if (!form[field] || !form[field].trim()) {
           _errors[field] = _errors[field] || [];
 
           _errors[field].push(requiredFields[field]);
+
+          hasError = true;
         }
       }
 
-      setErrors((errors) => _errors);
+      /* 약관 동의 체크 */
+      if (!form.agree) {
+        _errors.agree = _errors.agree || [];
+        _errors.push(t('AssertTrue_join_agree'));
+        hasError = true;
+      }
+
+      if (hasError) {
+        setErrors(() => _errors);
+
+        return;
+      }
+
+      // 회원가입 처리
     },
     [form],
   );
--- src/langs/ko/validation.js
+++ src/langs/ko/validation.js
@@ -3,6 +3,7 @@ const validation = {
   NotBlank_password: '비밀번호를 입력하세요.',
   NotBlank_confirmPassword: '비밀번호를 확인 하세요.',
   NotBlank_name: '회원명을 입력하세요.',
+  AssertTrue_join_agree: '회원가입 약관에 동의하세요.',
 };
 
 export default validation;
