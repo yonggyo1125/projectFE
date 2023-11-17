54d62f65f602357087beacdc48e0986ec1ecc30e
yonggyo1125 <yonggyo00@kakao.com>
2023-11-17 12:44:14 +0900

    .

--- src/containers/member/JoinContainer.js
+++ src/containers/member/JoinContainer.js
@@ -1,19 +1,66 @@
 import React, { useState, useCallback } from 'react';
+import { produce } from 'immer';
+import { useTranslation } from 'react-i18next';
 import JoinForm from '../../components/member/JoinForm';
+
 const JoinContainer = () => {
-  const [form, setForm] = useState({});
+  const { t } = useTranslation();
+
+  const [form, setForm] = useState({
+    agree: false,
+  });
   const [errors, setErrors] = useState({});
 
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      /**
+       * 필수 항목
+       */
+      const requiredFields = {
+        email: t('NotBlank_email'),
+        password: t('NotBlank_password'),
+        confirmPassword: t('NotBlank_confirmPassword'),
+        name: t('NotBlank_name'),
+      };
+      const _errors = {};
+
+      for (const field in requiredFields) {
+        if (!form[field] || !form[field].trim()) {
+          _errors[field] = _errors[field] || [];
+
+          _errors[field].push(requiredFields[field]);
+        }
+      }
+
+      setErrors((errors) => _errors);
+    },
+    [form],
+  );
+
+  const onChange = useCallback((e) => {
+    const target = e.currentTarget;
+    setForm(
+      produce((draft) => {
+        draft[target.name] = target.value;
+      }),
+    );
   }, []);
 
-  const onChange = useCallback(() => {}, []);
+  const onToggle = useCallback((e) => {
+    setForm(
+      produce((draft) => {
+        draft.agree = !draft.agree;
+      }),
+    );
+  }, []);
 
   return (
     <JoinForm
       onSubmit={onSubmit}
       onChange={onChange}
+      onToggle={onToggle}
       form={form}
       errors={errors}
     />
--- src/langs/ko/validation.js
+++ src/langs/ko/validation.js
@@ -1,3 +1,8 @@
-const validation = {};
+const validation = {
+  NotBlank_email: '이메일을 입력하세요.',
+  NotBlank_password: '비밀번호를 입력하세요.',
+  NotBlank_confirmPassword: '비밀번호를 확인 하세요.',
+  NotBlank_name: '회원명을 입력하세요.',
+};
 
 export default validation;
