9102861e280c1b7fbff32a8139eecb8dedbc9a0f
yonggyo1125 <yonggyo00@kakao.com>
2023-11-24 12:13:09 +0900

    .

--- src/containers/member/LoginContainer.js
+++ src/containers/member/LoginContainer.js
@@ -12,13 +12,32 @@ const LoginContainer = () => {
     (e) => {
       e.preventDefault();
 
+      let hasError = false;
+      const _errors = {};
+      setErrors(() => _errors);
+
       /* 필수 항목 검증 S */
       const requiredFields = {
         email: t('NotBlank_email'),
         password: t('NotBlank_password'),
       };
+
+      for (const field in requiredFields) {
+        if (!form[field] || !form[field].trim()) {
+          _errors[field] = requiredFields[field];
+          hasError = true;
+        }
+      }
       /* 필수 항목 검증 E */
+
+      if (hasError) {
+        setErrors(() => _errors);
+        return;
+      }
+
+      // 로그인 처리
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [form],
   );
 
