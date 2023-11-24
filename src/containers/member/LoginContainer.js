bf67e70edb81b45600d20a3777758d927e3da054
yonggyo1125 <yonggyo00@kakao.com>
2023-11-24 11:45:31 +0900

    .

--- src/containers/member/LoginContainer.js
+++ src/containers/member/LoginContainer.js
@@ -1,11 +1,27 @@
+import React, { useState, useCallback } from 'react';
 import LoginForm from '../../components/member/LoginForm';
 
 const LoginContainer = () => {
+  const [errors, setErrors] = useState({});
+  const [form, setForm] = useState({});
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    console.log(form);
+  }, []);
+
+  const onChange = useCallback((e) => {
+    setForm((form) => ({
+      ...form,
+      [e.currentTarget.name]: e.currentTarget.value,
+    }));
+  }, []);
+
   return (
     <>
-      <LoginForm />
+      <LoginForm onChange={onChange} onSubmit={onSubmit} errors={errors} />
     </>
   );
 };
 
-export default LoginContainer;
+export default React.memo(LoginContainer);
