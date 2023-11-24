233d99d457af8b6786b2fce7e65676b83f3574e6
yonggyo1125 <yonggyo00@kakao.com>
2023-11-24 11:51:13 +0900

    .

--- src/containers/member/LoginContainer.js
+++ src/containers/member/LoginContainer.js
@@ -1,14 +1,23 @@
 import React, { useState, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 import LoginForm from '../../components/member/LoginForm';
 
 const LoginContainer = () => {
   const [errors, setErrors] = useState({});
   const [form, setForm] = useState({});
 
+  const { t } = useTranslation();
+
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      console.log(form);
+
+      /* 필수 항목 검증 S */
+      const requiredFields = {
+        email: t('NotBlank_email'),
+        password: t('NotBlank_password'),
+      };
+      /* 필수 항목 검증 E */
     },
     [form],
   );
